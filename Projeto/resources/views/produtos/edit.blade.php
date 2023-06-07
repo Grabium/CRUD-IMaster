06d121c0ef91ce651fac397ec8c58ef85f1b348d
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2023-06-07 11:40:30 -0300

    edit.blade.php
    Correções de texto.

--- Projeto/resources/views/produtos/edit.blade.php
+++ Projeto/resources/views/produtos/edit.blade.php
@@ -4,13 +4,13 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Cadastrar um novo produto</title>
+        <title>Editar um novo produto</title>
     </head>
 
     <body>
         <form action="{{ route('alterar_produto', ['id'=>$produto->id]) }}" method="POST">
         @csrf
-            <legend>Crie um Novo Produto</legend>
+            <legend>Edite um Produto</legend>
             <label for="">Nome</label><br />
             <input type="text" name="nome" value="{{ $produto->nome }}"><br />
             <label for="">Custo</label><br />
